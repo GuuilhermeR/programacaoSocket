7a750e08515df9d7507d125573fe019834c1f07e
GuuilhermeR <guilherme.rudiger@hotmail.com>
2020-05-28 23:18:29 +0000

    Início do cliente socket

---
+++ client.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<sys/socket.h>
+
+int main(int agrc, char *argv[]) {
+
+    /* AF_INET - IPV4 ; SOCK_STREAM - TECP ; O - IP */
+    int socket_desc = socket(AF_INET, SOCK_STREAM, 0); 
+
+    if(socket_desc == -1){
+        printf("Não foi possível criar o socket\n");
+        return 1;
+    }
+
+    
+    return 0;
+}
