7cbd3c69e53cc5ff5adb3fa11a567ef1d4944bd7
GuuilhermeR <guilherme.rudiger@hotmail.com>
2020-05-29 00:57:05 +0000

    Servidor recebendo conexão do cliente

--- client.c
+++ client.c
@@ -1,61 +1,74 @@
-#include<stdio.h>
-#include<string.h> /* strlen */
-#include<sys/socket.h>
-#include<arpa/inet.h>  /* inet_addr */
-#include<unistd.h> /* close */
+#include <stdio.h>
+#include <string.h> /* strlen */
+#include <sys/socket.h>
+#include <arpa/inet.h> /* inet_addr */
+#include <unistd.h> /* close */
 
-int main(int agrc, char *argv[]) {
-
+int main(int argc, char *argv[])
+{
     int socket_desc;
     struct sockaddr_in server;
     char *message, server_reply[2000];
 
-
-    /* AF_INET - IPV4 ; SOCK_STREAM - TECP ; O - IP */
+    /* AF_INET - IPv4, SOCK_STREAM - tcp, 0 - IP */
     socket_desc = socket(AF_INET, SOCK_STREAM, 0); 
 
-    if(socket_desc == -1){
-        printf("Não foi possível criar o socket.\n");
-        return 1;
-    }
-    
-    
-    server.sin_addr.s_addr = inet_addr("172.217.28.132"); /* CONVERTER IP PARA LONG */
-    server.sin_family = AF_INET; /* IPV4 */ 
-    server.sin_port = htons(80); /* DEFINE A PORTA */
-
-    /* CONECTA-SE AO SERVIDOR */
-    if(connect(socket_desc, (struct sockaddr *) &server, sizeof(server)) < 0){
-        printf("Erro ao fazer conexão.\n");
+    if (socket_desc == -1)
+    {
+        printf("Não foi possível criar o socket\n");
         return 1;
     }
 
-        printf("Você está conectado.\n");
+    /* 
+    // IPv4 AF_INET sockets:
+    struct sockaddr_in {
+        short            sin_family;   // e.g. AF_INET, AF_INET6
+        unsigned short   sin_port;     // e.g. htons(3490)
+        struct in_addr   sin_addr;     // see struct in_addr, below
+        char             sin_zero[8];  // zero this if you want to
+    };
+    struct in_addr {
+        unsigned long s_addr;          // load with inet_pton()
+    };
+    struct sockaddr {
+        unsigned short    sa_family;    // address family, AF_xxx
+        char              sa_data[14];  // 14 bytes of protocol address
+    };
+    */
+    /* informa os dados do servidor para onde vamos nos conectar */
+    server.sin_addr.s_addr = inet_addr("127.0.0.1"); /* converte IP para long */
+    server.sin_family = AF_INET; /* ip v4 */
+    server.sin_port = htons(8888);
 
-
-    /* ENVIA DADOS */
-    message = "GET / HTTP/1.1\r\n\r\n";
-    if (send(socket_desc, message, strlen(message), 0) < 0 ){
-        printf("Erro ao enviar os dados.\n");
+    /* conecta-se ao servidor */
+    if (connect(socket_desc, (struct sockaddr *) &server, sizeof(server)) < 0)
+    {
+        printf("Erro ao conectar-se\n");
         return 1;
     }
 
+    printf("Conectado.\n");
 
+    /* envia dados */
+    message = "Olá, Mundo!";
+    if (send(socket_desc, message, strlen(message), 0) < 0)
+    {
+        printf("Erro ao enviar\n");
+        return 1;
+    }
     printf("Dados enviados.\n");
 
-    /* RECEBER DADOS */
-    if (recv(socket_desc, server_reply, 2000, 0) < 0){
-        printf("Erro ao receber os dados.\n");
+    /* recebe dados do servidor */
+    if (recv(socket_desc, server_reply, 2000, 0) < 0)
+    {
+        printf("Falha no recv\n");
         return 1;
     }
-
-    printf("Respostas recebida\n");
+    printf("Resposta recebida.\n");
     printf("%s\n", server_reply);
 
-    /* ENCERRA CONEXÃO */
+    /* encerra a conexão */
     close(socket_desc);
-
-    
 
     return 0;
 }
--- server.c
+++ server.c
@@ -1,35 +1,56 @@
+  
 #include <netinet/in.h>
-#include<stdio.h>
-#include<string.h> /* strlen */
-#include<sys/socket.h>
-#include<arpa/inet.h>  /* inet_addr */
-#include<unistd.h> /* close */
+#include <stdio.h>
+#include <string.h> /* strlen */
+#include <sys/socket.h>
+#include <arpa/inet.h> /* inet_addr */
+#include <unistd.h> /* close */
 
-int main(int agrc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+    int socket_desc, c, new_socket;
+    struct sockaddr_in server, client;
 
-    int socket_desc;
-    struct sockaddr_in server;
-
-    /* ABRE UM SOCKET */
+    /* abre um socket */
     socket_desc = socket(AF_INET, SOCK_STREAM, 0); 
-
-    if(socket_desc == -1){
-        printf("Não foi possível criar o socket.\n");
+    if (socket_desc == -1)
+    {
+        printf("Não foi possível criar o socket\n");
         return 1;
     }
 
-    /* ONDE QUERO OUVIR ? */
+    /* onde quero ouvir ? */
     server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY; /* ESCUTE EM QUALQUER IP DA MÁQUINA */
+    server.sin_addr.s_addr = INADDR_ANY; /* escute em qualquer IP da máquina */
     server.sin_port = htons(8888);
 
-    if ( bind(socket_desc, (struct sockaddr *) &server, sizeof(server)) < 0) {
-        printf("Erro ao fazer o bind.\n");
+    /* vincula o socket à porta e ao endereço informados */
+    if (bind(socket_desc, (struct sockaddr *) &server, sizeof(server)) < 0)
+    {
+        printf("Erro ao fazer o bind\n");
         return 1;
     }
 
     printf("Bind efetuado.\n");
 
-    return 0;
+    /* escutar */
+    listen(socket_desc, 3);
 
+    /* aceitar conexões entrantes */
+    printf("Aguardando conexões...\n");
+    c = sizeof(struct sockaddr_in);
+    new_socket = accept(socket_desc, (struct sockaddr *) &client, (socklen_t*) &c);
+    if (new_socket < 0) 
+    {
+        printf("Erro ao aceitar conexão\n");
+        return 1;
+    }
+
+    char *client_ip = inet_ntoa(client.sin_addr);
+    int client_port = ntohs(client.sin_port);
+
+    printf("conexão aceita do client %s:%d\n", client_ip, client_port);
+
+
+    return 0;
 }
