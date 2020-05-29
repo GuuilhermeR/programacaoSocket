ceb414e1cd0a6f34e59b691d5dfb06ab5aeede1b
GuuilhermeR <guilherme.rudiger@hotmail.com>
2020-05-29 00:16:16 +0000

    Enviar dados ao servidor

--- client.c
+++ client.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h> /* strlen */
 #include<sys/socket.h>
 #include<arpa/inet.h> 
 
@@ -6,6 +7,7 @@ int main(int agrc, char *argv[]) {
 
     int socket_desc;
     struct sockaddr_in server;
+    char *message;
 
     /* AF_INET - IPV4 ; SOCK_STREAM - TECP ; O - IP */
     socket_desc = socket(AF_INET, SOCK_STREAM, 0); 
@@ -15,17 +17,34 @@ int main(int agrc, char *argv[]) {
         return 1;
     }
     
-    server.sin_addr.s_addr = inet_addr("172.217.28.132"); /* Converter IP para long */
+    
+    server.sin_addr.s_addr = inet_addr("172.217.28.132"); /* CONVERTER IP PARA LONG */
     server.sin_family = AF_INET; /* IPV4 */ 
-    server.sin_port = htons(80); /* Define a porta */
+    server.sin_port = htons(80); /* DEFINE A PORTA */
 
     /* CONECTA-SE AO SERVIDOR */
     if(connect(socket_desc, (struct sockaddr *) &server, sizeof(server)) < 0){
-        printf("Erro ao fazer conexão.");
+        printf("Erro ao fazer conexão.\n");
         return 1;
     }
 
         printf("Você está conectado.\n");
+
+
+    /* ENVIA DADOS */
+    message = "GET / HTTP/1.1\r\n\r\n";
+    if (send(socket_desc, message, strlen(message), 0) < 0 ){
+        printf("Erro ao enviar.\n");
+        return 1;
+    }
+
+
+    printf("Dados enviados.\n");
+
+    /* RECEBER DADOS */
     
+
+
+
     return 0;
 }
